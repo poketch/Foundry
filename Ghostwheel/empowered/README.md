a965f7151ed5550fb6172c5bd7384198ef4e1691
J Palmer <j-mpalmer@live.com>
2022-08-28 09:43:33 -0400

    removed redundant items from save_buff and debuff. Macros now generate the necessary effect. Added example spells to illustrate both. added pearl of black doubt.

--- Ghostwheel/empowered/README.md
+++ Ghostwheel/empowered/README.md
@@ -1,19 +1,19 @@
 
 # Usage
 
-A series of items to make any saves imposed by an actor be made at disadvantage.
+A macro to make any saves imposed by an actor be made at disadvantage.
 
 
-Import the macro: `save_debuff` into a global macro and add the the item `Save Debuff` to desired actor. 
+Import the macro: `save_debuff` as global macro. 
 
-These can be used manually by calling the `save_debuff` macro from the item's OnUseMAcro and setting it to be called `After targeting complete`.
+This can be used manually by calling the `save_debuff` macro from an item's OnUseMacro and setting it to be called `After targeting complete` or any event prior to that. As shown with the `Strong Fireball` example item.
 
 These can also be combined into an effect as shown in the example `Empowered` feature which applies an effect which forces all imposed saves to be made at disadvantage.
 
-The particulars of the effect are unimportant the only important part of the set up the duration: `Expires if the character rolls: saving throw` and the effect's effects as shown in the picture below. 
+The particulars of the effect are unimportant the only important part of the set up are the effect's changes as shown in the picture below. 
 
 ![effect setup](./effect_setup.png)
 
 
-Should these be copied this functionality can be added to any effect on any item.
+Should these be copied this functionality can be added to any effect on any item or any item/feature/spell.
 
---
+++ Ghostwheel/pear-of-black-doubt/pearl-of-black-doubt.js
@@ -0,0 +1,56 @@
+const parent_eff = "Stance - Pearl of Black Doubt";
+
+const child_eff = "Doubt";
+
+
+const hook = Hooks.on("midi-qol.AttackRollComplete", main);
+
+async function main(args) {
+
+    //if the workflow is a spell, return
+    if (args.templateData != null || args.templateData != undefined) { return; }
+
+    const ttokens = [...args.targets]?.filter(token => [...token.actor.effects].some(ef => ef.data.label === parent_eff && ef.data.disabled === false));
+
+    if (ttokens.length == 0 || ttokens == null || ttokens == undefined) { return; }
+
+    for (const ttoken of ttokens) {
+
+        
+
+        //if target was hit, do nothing
+        if ([...args.hitTargets].includes(ttoken)) { continue; }
+
+        const tactor = ttoken.actor;
+
+        if ([...tactor.effects].some(ef => ef.data.label === child_eff)) { continue; }
+
+        //define effect
+        const effectData = {
+            label: child_eff,
+            icon: "modules/dfreds-convenient-effects/images/dodging.svg",
+            duration: { startTime: 120, turns: 1 },
+            flags: {
+                core: { statusId: " " },
+            },
+            changes: [
+                {
+                    key: "flags.midi-qol.grants.disadvantage.attack.all",
+                    mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM, value: "1"
+                },
+            ]
+        };
+
+        //apply effect
+        MidiQOL.socket().executeAsGM("createEffects", { actorUuid: tactor.uuid, effects: [effectData] });
+
+
+
+
+
+
+
+    }
+
+
+}
